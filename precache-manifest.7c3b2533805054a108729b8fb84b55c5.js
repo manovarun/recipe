739e6f3ca02973acbc3aa8d16dd18b90d2e4f181
manovarun <samvarun@gmail.com>
2020-01-16 21:03:46 +0530

    Updates

---
+++ precache-manifest.7c3b2533805054a108729b8fb84b55c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dae37a857903e18c8dc42ff8b03947ba",
+    "url": "/recipe/index.html"
+  },
+  {
+    "revision": "b98d8931dd9e7e7d570c",
+    "url": "/recipe/static/css/main.a67eae59.chunk.css"
+  },
+  {
+    "revision": "55b37daa757a7ddea80c",
+    "url": "/recipe/static/js/2.03804979.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/recipe/static/js/2.03804979.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b98d8931dd9e7e7d570c",
+    "url": "/recipe/static/js/main.04d2d579.chunk.js"
+  },
+  {
+    "revision": "806339b57b65526cdce1",
+    "url": "/recipe/static/js/runtime-main.5c0e36ed.js"
+  }
+]);
